7709dd37c70de2be0fc9508c4504636c067cc4d3
cutlerwater <cutlerwater2@live.com>
2021-10-18 14:28:31 -0400

    First commit

---
+++ newsApp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from newsapi import NewsApiClient
+# Create your views here.
+
+
+def index(request):
+    newsApi = NewsApiClient(api_key='6b8dd00129e14e9091fabdb20d342cfa')
+    headLines = newsApi.get_top_headlines(sources='ign, cnn')
+    articles = headLines['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        article = articles[i]
+        desc.append(article['description'])
+        news.append(article['title'])
+        img.append(article['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    return render(request, "main/index.html", context={"mylist": mylist})
+import urllib.request
+import json
+from django.shortcuts import render
+
+
+def weather(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                        city + '&units=metric&appid=99b0fa765a69852257a6bad630c1dff9').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "main/weather.html", data)
+
